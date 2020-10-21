5c69372081c2c1b62306ddc019a7004def1b1908
Andrejs Krilovs <andrejs.krilovs.0507@gmail.com>
2020-10-21 13:53:00 +0300

    Finished controller integration test

--- src/test/java/org/example/controller/UrlControllerTest.java
+++ src/test/java/org/example/controller/UrlControllerTest.java
@@ -10,10 +10,11 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.CoreMatchers.not;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,9 +25,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class UrlControllerTest {
 
     @Autowired
-    private UrlController controller;
-
-    @Autowired
     private MockMvc mockMvc;
 
     @Test
@@ -34,18 +32,58 @@ public class UrlControllerTest {
         mockMvc.perform(get("/"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(xpath("//*[@id='urlList']/tr").nodeCount(2));
+                .andExpect(xpath("//*[@id='urlList']/tr")
+                        .nodeCount(2));
     }
 
     @Test
-    public void generateUrl() {
+    public void generateUrlSuccess() throws Exception {
+        String url = "https://github.com/AndrejsKrilovs/UrlConverter";
+
+        mockMvc.perform(post("/")
+                .param("originalUrl", url))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(xpath("//*[@id='urlList']/tr").nodeCount(3))
+                .andExpect(xpath("//*[@id='urlList']/tr[1]/td[1]").exists())
+                .andExpect(xpath("//*[@id='urlList']/tr[1]/td[2]").string(url));
     }
 
     @Test
-    public void filterUrlValues() {
+    public void generateUrlFail() throws Exception {
+        String url = "https://www.github.com/AndrejsKrilovs/UrlConverter";
+
+        mockMvc.perform(post("/")
+                .param("originalUrl", url))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(xpath("//*[@id='urlList']/tr").nodeCount(2))
+                .andExpect(xpath("//*[@id='urlList']/tr[1]/td[2]").string(not(url)));
     }
 
     @Test
-    public void redirectToOriginalUrl() {
+    public void filterUrlValues() throws Exception {
+        mockMvc.perform(get("/filter").param("param", "goo"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(xpath("//*[@id='urlList']/tr").nodeCount(1))
+                .andExpect(xpath("//*[@id='urlList']/tr/td[2]").string("https://google.com"));
+    }
+
+    @Test
+    public void emptyFilter() throws Exception {
+        mockMvc.perform(get("/filter").param("param", ""))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(xpath("//*[@id='urlList']/tr").nodeCount(2))
+                .andExpect(xpath("//*[@id='urlList']/tr[1]/td[2]").string("https://spring.io/"));
+    }
+
+    @Test
+    public void redirectToOriginalUrl() throws Exception {
+        mockMvc.perform(get("/{key}", "htrd7jr"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("https://google.com"));
     }
 }
